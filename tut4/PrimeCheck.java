e62b32572525912ad401a2d4f393d0d6c91a479f
Minh <130217346+vminhh@users.noreply.github.com>
2023-12-02 21:27:59 +0700

    Update PrimeCheck

--- tut4/PrimeCheck.java
+++ tut4/PrimeCheck.java
@@ -1,19 +1,19 @@
-package Class.tut4;
 import java.util.*;
 
-public class PrimeCheck {
-    public static void main (String[] args){
-        Scanner inp = new Scanner(System.in);
-        int n = inp.nextInt();
-        inp.close();
-        System.out.printf("%s", (Product(n))? "It is a prime number." :"It is not a prime number.");
-    }
-    public static boolean Product(int n){
-        if (n<2 || n%2==0){
-            return false;
+public class Solve {
+    public static void main(String[] args) {
+        try (Scanner inp = new Scanner(System.in)) {
+            long n = inp.nextLong();
+            System.out.println(isPrime(n) ?"YES" :"NO");
         }
-        for (int i=2; i*i<=n; i++){
-            if (n%i==0)
+    }
+
+    static boolean isPrime(long n) {
+        if (n < 2 || (n > 2 && n % 2 == 0))
+            return false;
+
+        for (long i = 3; i * i <= n; i += 2) {
+            if (n % i == 0)
                 return false;
         }
         return true;
