7b859f5de3dc4115418e298a156fe02ec743d9bf
Minh <dovanminh253@gmail.com>
2023-12-12 22:48:08 +0700

    up 11

--- tut11/ClassManager.java
+++ tut11/ClassManager.java
@@ -1,11 +1,12 @@
 package Class.tut11;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ClassManager {
+public class ClassManager {   
     public static void main(String[] args) {
         Map<String, List<String>> classMap = new HashMap<>();
         Scanner inp = new Scanner(System.in);
@@ -21,10 +22,10 @@ public class ClassManager {
 
             switch (choice) {
                 case 1:
-                    addStudent();
+                    addStudent(classMap, inp);
                     break;
                 case 2:
-                    displayStudent();
+                    displayStudent(classMap, inp);
                     break;
                 case 3:
                     System.out.println("Exiting the program.....");
@@ -37,11 +38,31 @@ public class ClassManager {
         }while(choice != 3);
     }
 
-    private static void addStudent (){
+    private static void addStudent (Map<String, List<String>> classMap, Scanner inp){
+        System.out.print("Enter the class name: ");
+        String className = inp.nextLine();
 
+        List<String> student = classMap.getOrDefault(className, new ArrayList<>());
+        System.out.print("Enter the name of student: ");
+        String studentName = inp.nextLine();
+
+        student.add(studentName);
+        classMap.put(className, student);
+        System.out.println("Student added to the class successfully.");
     }
 
-    private static void displayStudent (){
+    private static void displayStudent (Map<String, List<String>> classMap, Scanner inp){
+        System.out.print("Enter the class name: ");
+        String className = inp.nextLine();
 
+        List<String> students = classMap.get(className);
+
+        if (students != null){
+            System.out.println("Students in class " + className + ":");
+            for (String student : students ){
+                System.out.print(student + " ");
+            }
+        }
+        else    System.out.println("Class not found......");
     }
 }
